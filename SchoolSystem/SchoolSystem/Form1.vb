d2337b7a8ed6f03c441af40dc05d2b0aa8c1e927
ebaham <ebaum29@gmail.com>
2017-03-31 14:50:46 -0500

    Add files via upload
    System so far

---
+++ SchoolSystem/SchoolSystem/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If UserBox.Text = "rfb" And PassWBox.Text = "reflection" Then
+            Form2.Show()
+            Me.Close()
+        Else
+            MsgBox("Sorry, username or password not found", MsgBoxStyle.OkOnly, "Invalid")
+        End If
+    End Sub
+
+End Class
